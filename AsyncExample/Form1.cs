0897a384b1615cd9bd27691bae92e91118e2cd6d
Kristoffer K <Kristoffer92k@hotmail.com>
2020-03-08 14:29:33 +0100

    Adding remaining changes

--- AsyncExample/Form1.cs
+++ AsyncExample/Form1.cs
@@ -16,5 +16,31 @@ namespace AsyncExample
         {
             InitializeComponent();
         }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+            
+            CountFiveSeconds();
+            textBox1.Text += "Waiting for method" + Environment.NewLine;
+            
+        }
+
+        private async Task CountFiveSeconds()
+        {
+            textBox1.Text = "Counting to Three" + Environment.NewLine;
+            string text = await Task.Run(() => DoTheSleep(3));
+            textBox1.Text += text;
+
+        }
+
+        private String DoTheSleep(int i)
+        {
+            for (int a = 0; a < i; a++)
+            {
+                System.Threading.Thread.Sleep(1000);
+
+            }
+            return "Done";
+        }
     }
 }
